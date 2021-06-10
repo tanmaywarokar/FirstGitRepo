525febabd29593893075de3f44cdfec2db9c1573
Tanmay Warokar <warokart54@gmail.com>
2021-06-10 22:39:54 +0530

    Commiting

---
+++ src/main/java/in/nareshit/raghu/FirstGitRepoApplication.java
@@ -0,0 +1,13 @@
+package in.nareshit.raghu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstGitRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstGitRepoApplication.class, args);
+	}
+
+}
