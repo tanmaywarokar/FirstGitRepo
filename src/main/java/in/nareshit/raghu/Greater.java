f654dd001652b4a7e7ee031c36feb311a597a766
Tanmay Warokar <warokart54@gmail.com>
2021-06-11 08:38:41 +0530

    FG-2589 Update new Greater.class

---
+++ src/main/java/in/nareshit/raghu/Greater.java
@@ -0,0 +1,9 @@
+package in.nareshit.raghu;
+
+public class Greater {
+
+	public static void main(String[] args) {
+      System.out.println("I am Tanmay.");
+	}
+
+}
--- src/main/java/in/nareshit/raghu/controller/HomeController.java
+++ src/main/java/in/nareshit/raghu/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/home")
 public class HomeController {
-
+	
 	@GetMapping("/welcome")
 	public String welcome() {
 		return "welcome.html";
