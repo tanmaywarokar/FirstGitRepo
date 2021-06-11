42229f2d797236d91abd1f62220a0b1c6ffc45f1
Tanmay Warokar <warokart54@gmail.com>
2021-06-11 08:51:42 +0530

    FG-25645 Greater.class updated

--- src/main/java/in/nareshit/raghu/Greater.java
+++ src/main/java/in/nareshit/raghu/Greater.java
@@ -2,8 +2,24 @@ package in.nareshit.raghu;
 
 public class Greater {
 
+	public static void greater(int a,int b, int c) {
+		if(a>b && a>c) {
+			System.out.println("a is greater");
+		}
+		else if(b>a && b>c) {
+			System.out.println("b is greater");
+		}
+		else if(c>a && c>b) {
+			System.out.println("c is greater");
+		}
+		else{
+			System.out.println("all are equal");
+		}
+		
+	}
+	
 	public static void main(String[] args) {
-      System.out.println("I am Tanmay.");
+      Greater.greater(10, 20, 30);
 	}
 
 }
